e8a5a5ba03ba08cf8f01f869c0f450f5230c51e8
Tuong <lengoctuong23052002@gmail.com>
2022-07-16 13:31:47 +0700

    [02_model]

---
+++ model/model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const authorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number
+    },
+    books: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Book'
+        }
+    ]
+})
+
+const bookSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    pulishedDate: {
+        type: String
+    },
+    genres: {
+        type: [String]
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Author'
+    }
+})
+
+let Author = mongoose.model('Author', authorSchema)
+let Book = mongoose.model('Book', bookSchema)
+module.exports = { Author, Book }
